b1ffb0a83e774e50c218ae30a734e40b4bf764f3
bijuPrakash <71547234+bijuPrakash@users.noreply.github.com>
2020-09-19 15:28:30 +0530

    Added square function in the validation.py
    square function will find the square of a number

--- validations.py
+++ validations.py
@@ -1,1 +1,7 @@
-#yo buzz i am sing a song--which song is this can anyone answer
+#A fanshun to find the square of a number
+#!/usr/bin/env python
+
+def square(num):
+    print("squared of that number is ",num*2)
+
+square(98.89)
