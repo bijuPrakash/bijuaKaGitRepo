fefd173f8629f3115d594436725ad9bb8ad9051d
AjeetPrakashMIexpert <48152003+AjeetPrakashMIexpert@users.noreply.github.com>
2020-09-19 15:50:36 +0530

    corrected square function in validation.py
    fanshion was replaced with function in the comment line and
    num*2 was replaced by num**2 in the codeline

--- validations.py
+++ validations.py
@@ -1,7 +1,7 @@
-#A fanshun to find the square of a number
+#A function to find the square of a number
 #!/usr/bin/env python
 
 def square(num):
-    print("squared of that number is ",num*2)
+    print("squared of that number is ",num**2)
 
 square(98.89)
